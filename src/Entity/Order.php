efed568c06068defe596250d53ef19abc38dcbb3
ChewyPegasus <neveroyatnyi.chelovek@gmail.com>
2025-05-30 09:45:33 +0800

    added order and enum-river

---
+++ src/Entity/Order.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?\DateInterval $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateInterval
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateInterval $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
---
+++ src/Enum/River.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enum;
+
+enum River: string {
+    case SVISLOCH = 'svisloch';
+    case ISLOCH = 'isloch';
+    case UZLYANKA = 'uzlyanka';
+    case NAROCHANKA = 'narochanka';
+    case STRACHA = 'stracha';
+    case SARYANKA = 'saryanka';
+    case SLUCH = 'sluch';
+    case VILIYA = 'viliya';
+    case SULA = 'sula';
+    case USA = 'usa';
+    case SMERD = 'smerd';
+}
