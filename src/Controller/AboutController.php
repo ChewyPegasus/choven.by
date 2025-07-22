f3ef65893551a632d86beb118d12d85efe87914d
ChewyPegasus <neveroyatnyi.chelovek@gmail.com>
2025-07-22 12:17:46 +0300

    made photogallery to autoload files from directory

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Controller\Trait\CacheableTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,6 +17,19 @@ final class AboutController extends AbstractController
     #[Route('/about', name: 'app_about')]
     public function index(): Response
     {
-        return $this->createCacheableResponse('about/index.html.twig');
+        $galleryPath = $this->getParameter('kernel.project_dir') . '/assets/images/gallery';
+        $images = [];
+
+        if (is_dir($galleryPath)) {
+            $finder = new Finder();
+            $finder->files()->in($galleryPath)->sortByName();
+
+            foreach ($finder as $file) {
+                $images[] = 'images/gallery/' . $file->getRelativePathname();
+            }
+        }
+        return $this->createCacheableResponse('about/index.html.twig', [
+            'gallery_images' => $images,
+        ]);
     }
 }
